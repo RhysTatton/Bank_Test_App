041a7a221717513a17219296d66f5263c59ea413
Rhys Tatton <rhystatton98@gmail.com>
2023-10-19 19:45:41 +0100

    added a basic menu to the BankTest

--- BankTEST_Main.py
+++ BankTEST_Main.py
@@ -1,37 +1,12 @@
 import sqlite3
 import hashlib
 import requests
-from flask import Flask
-
-app = Flask(__name__, instance_relative_config=True)
-
-
-@app.route('/BankTest')
-def register_user():
-    db = sqlite3.connect('basic_bank.db')
-    cursor = db.cursor()
-    try:
-        reg_username = input("\nPlease enter a username: ")
-        reg_password = input("\nPlease enter a password: ")
-        reg_balance = float(input("\nPlease enter how much you wish to put into the account on start up: "))
-
-        password_hash = hashlib.sha256(reg_password.encode()).hexdigest()
-
-        user = [reg_username, password_hash, reg_balance]
-
-        cursor.execute(''' INSERT INTO users(username, password_hash, balance) VALUES(?,?,?)''', user)
-        db.commit()
-    except (ValueError, Exception) as e:
-        print("\n Error adding user", e)
-        db.rollback()
-    db.close()
 
 
 def user_login():
     db = sqlite3.connect('basic_bank.db')
     cursor = db.cursor()
     user_id = None
-
     try:
         username = input("\nUsername: ")
         password = input("\nPassword: ")
@@ -56,6 +31,26 @@ def user_login():
         print("\nAn error occurred:", str(e))
 
     return user_id
+
+
+def register_user():
+    db = sqlite3.connect('basic_bank.db')
+    cursor = db.cursor()
+    try:
+        reg_username = input("\nPlease enter a username: ")
+        reg_password = input("\nPlease enter a password: ")
+        reg_balance = float(input("\nPlease enter how much you wish to put into the account on start up: "))
+
+        password_hash = hashlib.sha256(reg_password.encode()).hexdigest()
+
+        user = [reg_username, password_hash, reg_balance]
+
+        cursor.execute(''' INSERT INTO users(username, password_hash, balance) VALUES(?,?,?)''', user)
+        db.commit()
+    except (ValueError, Exception) as e:
+        print("\n Error adding user", e)
+        db.rollback()
+    db.close()
 
 
 def transfer(sender_id, receiver_id, amount):
@@ -120,3 +115,30 @@ def check_balance(user_id):
 
     except sqlite3.Error as e:
         print("\nDatabase error:", str(e))
+
+
+try:
+    initial_menu = int(input('''\nWelcome to the BankTest App!
+    
+                                Please enter the number for what you wish to do: 
+                                
+                                1. Login
+                                2. Register
+                                3. Quit
+                                
+                                > '''))
+
+    if initial_menu == 1:
+        user_login()
+
+    elif initial_menu == 2:
+        register_user()
+
+    elif initial_menu == 3:
+        print("\nThank you for using the BankTest App")
+
+    else:
+        print("\nYou have entered an invalid input")
+
+except ValueError as e:
+    print("\nYou have entered an invalid input")
